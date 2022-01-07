f2fd366810889f024e51d0ef3c8d917da327fcd2
shafayetimam <shafayet@imams.me>
2022-01-06 19:35:41 -0500

    Update index.tsx
    removed the news bar

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,6 @@ import Experience from "../components/experience";
 import ProfileHero from "../components/profile";
 import Recommendation from "../components/recomendations";
 import PopularPosts from "../components/popularposts";
-import News from "../components/newsbar";
 import FAQ from "../components/faq"
 import Layout from "../components/layout";
 
@@ -11,7 +10,6 @@ import Layout from "../components/layout";
 export default function IndexPage() {
     return (
      <Layout>
-        <News />
         <ProfileHero />
         <TrustMe />
         <Recommendation />
